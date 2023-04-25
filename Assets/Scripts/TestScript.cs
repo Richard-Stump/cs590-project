96555e3cff5b7fde8ccb455946cb7bae457c1fde
Richard-Stump <rnstump@purdue.edu>
2023-04-25 18:03:02 -0400

    Get basic scene understanding working

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,18 +1,100 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Experimental.SpatialAwareness;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
 
-public class TestScript : MonoBehaviour
+public class TestScript : MonoBehaviour, IMixedRealitySpatialAwarenessObservationHandler<SpatialAwarenessSceneObject>
 {
-    // Start is called before the first frame update
-    void Start()
+    private IMixedRealitySceneUnderstandingObserver observer;
+
+
+    private List<GameObject> instantiatedPrefabs;
+
+    private Dictionary<SpatialAwarenessSurfaceTypes, Dictionary<int, SpatialAwarenessSceneObject>> observedSceneObjects;
+
+    private bool isRegistered = false;
+
+    #region MonoBehaviour Functions
+
+    protected void Start()
     {
-        
+        observer = CoreServices.GetSpatialAwarenessSystemDataProvider<IMixedRealitySceneUnderstandingObserver>();
+
+        if (observer == null)
+        {
+            Debug.LogError("Couldn't access Scene Understanding Observer! Please make sure the current build target is set to Universal Windows Platform. "
+                + "Visit https://docs.microsoft.com/windows/mixed-reality/mrtk-unity/features/spatial-awareness/scene-understanding for more information.");
+            return;
+        }
+
+        observer.Enable();
+        observer.RequestPlaneData = true;
+
+        instantiatedPrefabs = new List<GameObject>();
+        observedSceneObjects = new Dictionary<SpatialAwarenessSurfaceTypes, Dictionary<int, SpatialAwarenessSceneObject>>();
+    }
+    protected void OnEnable()
+    {
+        RegisterEventHandlers<IMixedRealitySpatialAwarenessObservationHandler<SpatialAwarenessSceneObject>, SpatialAwarenessSceneObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void OnDisable()
     {
-        
+        UnregisterEventHandlers<IMixedRealitySpatialAwarenessObservationHandler<SpatialAwarenessSceneObject>, SpatialAwarenessSceneObject>();
+    }
+
+    protected void OnDestroy()
+    {
+        UnregisterEventHandlers<IMixedRealitySpatialAwarenessObservationHandler<SpatialAwarenessSceneObject>, SpatialAwarenessSceneObject>();
+    }
+
+    #endregion MonoBehaviour Functions
+
+    /// <summary>
+    /// Registers for the spatial awareness system events.
+    /// </summary>
+    protected virtual void RegisterEventHandlers<T, U>()
+        where T : IMixedRealitySpatialAwarenessObservationHandler<U>
+        where U : BaseSpatialAwarenessObject
+    {
+        if (!isRegistered && (CoreServices.SpatialAwarenessSystem != null))
+        {
+            Debug.Log("TestScript: Registered to Scene Understanding");
+
+            CoreServices.SpatialAwarenessSystem.RegisterHandler<T>(this);
+            isRegistered = true;
+        }
+    }
+
+    /// <summary>
+    /// Unregisters from the spatial awareness system events.
+    /// </summary>
+    protected virtual void UnregisterEventHandlers<T, U>()
+        where T : IMixedRealitySpatialAwarenessObservationHandler<U>
+        where U : BaseSpatialAwarenessObject
+    {
+        if (isRegistered && (CoreServices.SpatialAwarenessSystem != null))
+        {
+            Debug.Log("TestScript: Unregistered from Scene Understanding");
+
+            CoreServices.SpatialAwarenessSystem.UnregisterHandler<T>(this);
+            isRegistered = false;
+        }
+    }
+
+    public void OnObservationAdded(MixedRealitySpatialAwarenessEventData<SpatialAwarenessSceneObject> eventData)
+    {
+        Debug.Log("TestScript: Observation Added");
+    }
+
+    public void OnObservationUpdated(MixedRealitySpatialAwarenessEventData<SpatialAwarenessSceneObject> eventData)
+    {
+        Debug.Log("TestScript: Observation Updated");
+    }
+
+    public void OnObservationRemoved(MixedRealitySpatialAwarenessEventData<SpatialAwarenessSceneObject> eventData)
+    {
+        Debug.Log("TestScript: Observation Removed");
     }
 }
